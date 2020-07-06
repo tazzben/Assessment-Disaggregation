4e437d0af2f8a7eb2366302f666ba8aa62ffc6c9
Ben Smith <ben@bensresearch.com>
2020-07-06 18:20:10 -0500

    Switch to .env values

--- forge.config.js
+++ forge.config.js
@@ -1,42 +1,19 @@
-const keychain = require('keytar');
-
-
-async function getGitHub (){
-   let credentials = await keychain.findCredentials("githubAPIToken");
-   let token = "";
-   for (let row of credentials){
-     token = row.password;
-   }
-   return token;
-}
-
-async function getAppleId (){
-  let credentials = await keychain.findCredentials("electronBuildAppleID");
-  let cert = await keychain.findCredentials("electronBuildCert");
-  let notarize = {};
-  let useCert = "";
-  for (let row of credentials){
-    notarize = {
-     appleId: row.account,
-     appleIdPassword: row.password
-    };
-  }
-  for (let row of cert){
-    useCert = row.password;
-  }
- return {notarize: notarize, useCert: useCert}; 
-}
-
+require('dotenv').config({ path: '~/.env' });
 let baseConfig  = {
   packagerConfig: {
       icon: "./src/icons/icon.icns",
       appId: "com.bensresearch.assessmentdisaggregation",
       appBundleId: "com.bensresearch.assessmentdisaggregation",
       osxSign: {
+        identity: process.env.APPLE_CERT,
         hardenedRuntime: true,
         entitlements: "entitlements.plist",
         "entitlements-inherit": "entitlements.plist",
         "gatekeeper-assess": false
+      },
+      osxNotarize: {
+        appleId: process.env.APPLEID,
+        appleIdPassword: process.env.APPLEID_PASSWORD
       }
   },
   makers: [
@@ -71,33 +48,16 @@ let baseConfig  = {
           config: {}
         }
   ],
-  publishers:[]
+  publishers:[{
+    name: '@electron-forge/publisher-github',
+    config: {
+      repository: {
+        owner: 'tazzben',
+        name: 'Assessment-Disaggregation'
+      },
+      authToken: process.env.GITHUB_TOKEN
+    }
+  }]
 };
 
-if(process.platform === 'darwin'){
-  let creds = getAppleId();
-  creds.then((value) => {
-    const {notarize, useCert} = value;
-    baseConfig.packagerConfig.osxNotarize = notarize;
-    baseConfig.packagerConfig.osxSign.identity = useCert;
-  });
-}
-
-if (process.platform !== "win32" ){
-  let github = getGitHub();
-  github.then((value) => {
-    let githubconfig = {
-      name: '@electron-forge/publisher-github',
-      config: {
-        repository: {
-          owner: 'tazzben',
-          name: 'Assessment-Disaggregation'
-        }
-      },
-      authToken: value
-    };
-    baseConfig.publishers.push(githubconfig);
-  });
-}
-
-module.exports =  baseConfig;
+module.exports = baseConfig;
