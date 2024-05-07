4d751b079e681c6e2f0eacf8bb30374478d652a5
Ben Smith <ben@bensresearch.com>
2024-05-07 05:49:43 -0500

    Fix footer and add icons

--- forge.config.js
+++ forge.config.js
@@ -26,6 +26,8 @@ let baseConfig = {
   makers: [{
       name: "@electron-forge/maker-squirrel",
       config: {
+        iconUrl: './src/icons/icon.ico',
+        setupIcon: './src/icons/icon.ico',
         signWithParams: '/a /tr http://timestamp.sectigo.com /td sha256 /fd sha256'
       },
       platforms: [
