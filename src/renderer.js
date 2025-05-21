c0483c6f98a2301b5c5ed7346824cb31923780b6
Ben Smith <ben@bensresearch.com>
2025-05-21 07:44:05 -0500

    Upgrade boostrap, fix tooltip bug

--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,12 @@
+const updateTooltip = (selector, title) => {
+    $(selector).attr("title", title);
+    $(selector).attr("data-original-title", title);
+    const tooltip  = bootstrap.Tooltip.getInstance(selector);
+    if (tooltip) {
+        tooltip.setContent({ '.tooltip-inner': title });
+    }
+};
+
 window.api.receive("fromMain", (data) => {
     if (data.students != undefined) {
         let numberofStudents = Math.round(data.students);
@@ -31,58 +40,38 @@ window.api.receive("fromMain", (data) => {
     }
     if (data.examFileOne && data.examFileOne.length > 0) {
         $("#pretestFile").text(" - Loaded");
-        $("#pretest").attr("title", "\"" + data.examFileOne + "\" is loaded as the pretest");
-        $("#pretest").attr("data-original-title", "\"" + data.examFileOne + "\" is loaded as the pretest");
-        $("#pretest").tooltip('update');
+        updateTooltip("#pretest", "\"" + data.examFileOne + "\" is loaded as the pretest");
         if (!data.startup) {
             $("#pretest").tooltip('show');
         }
     }
     if (data.examFileTwo && data.examFileTwo.length > 0) {
         $("#posttestFile").text(" - Loaded");
-        $("#posttest").attr("title", "\"" + data.examFileTwo + "\" is loaded as the posttest");
-        $("#posttest").attr("data-original-title", "\"" + data.examFileTwo + "\" is loaded as the posttest");
-        $("#posttest").tooltip('update');
+        updateTooltip("#posttest", "\"" + data.examFileTwo + "\" is loaded as the posttest");
         if (!data.startup) {
             $("#posttest").tooltip('show');
         }
     }
     if (data.assessmentFile && data.assessmentFile.trim().length > 0) {
         $("#assessmentMapExtra").hide();
-        $("#assessmentMap").attr("title", "\"" + data.assessmentFile + "\" is loaded as the assessment map");
-        $("#assessmentMap").attr("data-original-title", "\"" + data.assessmentFile + "\" is loaded as the assessment map");
-        $("#assessmentMap").tooltip('update');
-        $("#assessmentFileText").attr("title", "\"" + data.assessmentFile + "\" is loaded as the assessment map");
-        $("#assessmentFileText").attr("data-original-title", "\"" + data.assessmentFile + "\" is loaded as the assessment map");
-        $("#assessmentFileText").tooltip('update');
+        updateTooltip("#assessmentMap", "\"" + data.assessmentFile + "\" is loaded as the assessment map");
+        updateTooltip("#assessmentFileText", "\"" + data.assessmentFile + "\" is loaded as the assessment map");
     } else if (data.assessmentFile) {
         $("#assessmentMapExtra").show();
-        $("#assessmentMap").attr("title", "Click to load the assessment map file");
-        $("#assessmentMap").attr("data-original-title", "Click to load the assessment map file");
-        $("#assessmentMap").tooltip('update');
-        $("#assessmentFileText").attr("title", "No assessment map is loaded");
-        $("#assessmentFileText").attr("data-original-title", "No assessment map is loaded");
-        $("#assessmentFileText").tooltip('update');
+        updateTooltip("#assessmentMap", "Click to load the assessment map file");
+        updateTooltip("#assessmentFileText", "No assessment map is loaded");
     }
     if (!data.startup && data.assessmentFile) {
         $("#assessmentMap").tooltip('show');
     }
     if (data.studentFile && data.studentFile.trim().length > 0) {
         $("#studentListExtra").hide();
-        $("#studentList").attr("title", "\"" + data.studentFile + "\" is loaded as the student list");
-        $("#studentList").attr("data-original-title", "\"" + data.studentFile + "\" is loaded as the student list");
-        $("#studentList").tooltip('update');
-        $("#studentFileText").attr("title", "\"" + data.studentFile + "\" is loaded as the student list");
-        $("#studentFileText").attr("data-original-title", "\"" + data.studentFile + "\" is loaded as the student list");
-        $("#studentFileText").tooltip('update');
+        updateTooltip("#studentList", "\"" + data.studentFile + "\" is loaded as the student list");
+        updateTooltip("#studentFileText", "\"" + data.studentFile + "\" is loaded as the student list");
     } else if (data.studentFile) {
         $("#studentListExtra").show();
-        $("#studentList").attr("title", "Click to load the student list file");
-        $("#studentList").attr("data-original-title", "Click to load the student list file");
-        $("#studentList").tooltip('update');
-        $("#studentFileText").attr("title", "No student list is loaded");
-        $("#studentFileText").attr("data-original-title", "No student list is loaded");
-        $("#studentFileText").tooltip('update');
+        updateTooltip("#studentList", "Click to load the student list file");
+        updateTooltip("#studentFileText", "No student list is loaded");
     }
     if (!data.startup && data.studentFile) {
         $("#studentList").tooltip('show');
